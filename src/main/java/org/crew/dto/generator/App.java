f2e1aa3a9afef0dbb1620cdfb45ab97c0a72ed99
Alexandre Guimarães Rolim <alexandreg.rolim@saude.gov.br>
2017-08-14 16:37:08 -0300

    Comitando o projeto

---
+++ src/main/java/org/crew/dto/generator/App.java
@@ -0,0 +1,13 @@
+package org.crew.dto.generator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/org/crew/dto/generator/annotation/DTO.java
@@ -0,0 +1,5 @@
+package org.crew.dto.generator.annotation;
+
+public @interface DTO {
+
+}
